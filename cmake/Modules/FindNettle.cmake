e81ab9d3aa2b68144652b73b5e9082cb0f4f8a0a
Paul-Louis Ageneau <paul-louis@ageneau.org>
2020-01-16 18:45:34 +0100

    Added CMakeLists

---
+++ cmake/Modules/FindNettle.cmake
@@ -0,0 +1,17 @@
+if (NOT TARGET Nettle::Nettle)
+	find_path(NETTLE_INCLUDE_DIR nettle/sha.h)
+	find_library(NETTLE_LIBRARY NAMES nettle libnettle)
+
+	include(FindPackageHandleStandardArgs)
+	find_package_handle_standard_args(NETTLE DEFAULT_MSG NETTLE_LIBRARY NETTLE_INCLUDE_DIR)
+
+    if (NETTLE_FOUND)
+        add_library(Nettle::Nettle UNKNOWN IMPORTED)
+        set_target_properties(Nettle::Nettle PROPERTIES
+            IMPORTED_LOCATION "${NETTLE_LIBRARY}"
+            INTERFACE_INCLUDE_DIRECTORIES "${NETTLE_INCLUDE_DIRS}"
+            INTERFACE_LINK_LIBRARIES "${NETTLE_LIBRARIES}"
+                IMPORTED_LINK_INTERFACE_LANGUAGES "C")
+    endif ()
+endif ()
+
