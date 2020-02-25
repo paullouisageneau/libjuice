ef836242e5254eea3018b1d1ff70a88c440dfd7d
Paul-Louis Ageneau <paul-louis@ageneau.org>
2020-02-25 20:42:15 +0100

    Properly limit records count to MAX_STUN_SERVER_RECORDS_COUNT

--- src/agent.c
+++ src/agent.c
@@ -291,6 +291,8 @@ void agent_run(juice_agent_t *agent) {
 		int records_count = addr_resolve(agent->config.stun_server_host, service, records,
 		                                 MAX_STUN_SERVER_RECORDS_COUNT);
 		if (records_count > 0) {
+			if (records_count > MAX_STUN_SERVER_RECORDS_COUNT)
+				records_count = MAX_STUN_SERVER_RECORDS_COUNT;
 			JLOG_VERBOSE("Sending STUN binding request to %zu server addresses", records_count);
 			for (int i = 0; i < records_count; ++i) {
 				if (agent->entries_count >= MAX_STUN_ENTRIES_COUNT)
