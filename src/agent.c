3316f4c0b0fc43b3d4d73f0a2b87d72e2eed25ed
Paul-Louis Ageneau <paul-louis@ageneau.org>
2020-09-19 15:36:05 +0200

    Added handling for ECONNREFUSED and ENETRESET

--- src/agent.c
+++ src/agent.c
@@ -450,10 +450,11 @@ int agent_recv(juice_agent_t *agent) {
 		int len = recvfrom(agent->sock, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&record.addr,
 		                   &record.len);
 		if (len < 0) {
-			if (sockerrno == SECONNRESET) {
+			if (sockerrno == SECONNREFUSED || sockerrno == SECONNRESET || sockerrno == SENETRESET) {
 				// On Windows, if a UDP socket receives an ICMP port unreachable response after
 				// sending a datagram, this error is stored, and the next call to recvfrom() returns
-				// WSAECONNRESET. Therefore, it may be ignored.
+				// WSAECONNRESET (port unreachable) or WSAENETRESET (TTL expired).
+				// Therefore, it may be ignored.
 				continue;
 			}
 			if (sockerrno == SEAGAIN || sockerrno == SEWOULDBLOCK) {
