2562d2920f115574c2c81bfd585356d5f66a7ef5
0x34d <ajsinghyadav00@gmail.com>
2022-08-14 09:37:38 +0530

    change copyright
    Signed-off-by: 0x34d <ajsinghyadav00@gmail.com>

--- fuzzer/fuzzer.c
+++ fuzzer/fuzzer.c
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2020 Paul-Louis Ageneau
+ * Copyright (c) 2022 0x34d
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
